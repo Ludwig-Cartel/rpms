8f27afa1f01211c6b44970bbe1375e58842d270a
Ludwig Cartel <cartel.ludwig@gmail.com>
2020-03-03 21:58:03 +0800

    vanilla

---
+++ resource/php/class/db/config.php
@@ -0,0 +1,18 @@
+<?php
+ class config {
+   private $user = 'root';
+   private $pass = '';
+   public $pdo = null;
+
+   public function connect(){
+     try{
+       $this->pdo = new PDO('mysql:host=localhost;dbname=rpms', $this->user, $this->pass);
+       $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+       $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+     } catch (PDOException $e) {
+       print "Error!: " . $e->getMessage() . '<br />';
+     }
+     return $this->pdo;
+   }
+ }
+?>
